1d121227168aa65e8fc48fec2ddf329eb546d3fc
harveyywang <harveyywang@tencent.com>
2021-12-22 13:04:38 +0800

    add: 切片SumAll

--- array/sum_test.go
+++ array/sum_test.go
@@ -1,18 +1,16 @@
 package array
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestSum(t *testing.T) {
+func TestSumAll(t *testing.T) {
 
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+	got := SumAll([]int{1,2}, []int{0,9})
+	want := []int{3, 9}
 
-		got := Sum(numbers)
-		want := 15
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
-
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
