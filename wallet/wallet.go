77f36ee3625e7bd9f52be5f6879f71c8ad7e85be
harveyywang <harveyywang@tencent.com>
2021-12-27 13:18:42 +0800

    add: 指针和错误

---
+++ wallet/wallet.go
@@ -0,0 +1,39 @@
+package wallet
+
+import (
+	"errors"
+	"fmt"
+)
+
+var InsufficientFundsError = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Println("address of balance in Deposit is", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance{
+		return InsufficientFundsError
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Stringer interface {
+	String() string
+}
