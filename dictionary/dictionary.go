873a482caca7a5b3449df6c87139ac4dd65b3612
harveyywang <harveyywang@tencent.com>
2021-12-28 13:10:57 +0800

    add: Map学习一

---
+++ dictionary/dictionary.go
@@ -0,0 +1,19 @@
+package dictionary
+
+import "errors"
+
+var ErrorNotFound = errors.New("could not find the word you were looking for")
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrorNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word ,definition string)  {
+	d[word] = definition
+}
